29a7c850a6a71a1f8ec3a7ead484891320a384cb
mansikapuriy <156275635+mansikapuriy@users.noreply.github.com>
2024-01-11 09:08:30 +0530

    Add files via upload

---
+++ string_fun.php
@@ -0,0 +1,30 @@
+<?php
+//$a=strlen("Hello, world!");
+//echo $a;
+
+//$b=strpos("hello, world!","world");
+//echo $b;
+
+//$c=str_replace("world","universe","hello,world");
+//echo $c;
+
+//$b=substr("hello, world!",0,5);
+//echo $b;
+
+//$e=strtolower("HELLO WORLD!");
+//echo $e;
+
+//$f=strtoupper("hello, world!");
+//echo $f;
+
+//$g=ucfirst("hello, world!");
+//echo $g;
+
+//$h=strrev("hello, world!");
+//echo $h;
+
+$i=trim("Hello, World!");
+echo $i;
+?>
+
+
